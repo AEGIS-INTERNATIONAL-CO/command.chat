9b5b9ef5ecdc48ca2e1c0ac7340db0f11481ddbe
DarkBYT153#4092 <91132683+ShadowPlazs@users.noreply.github.com>
2021-12-28 12:00:34 +0800

    Update command.chatSetup.py

--- command.chatSetup.py
+++ command.chatSetup.py
@@ -1,1 +1,15 @@
+import chat
+from chat import commands
 
+def on_ready(rtx):
+  await channel.message.send("Successfully connected to 0.user.".format(Client))
+
+def on_message("/c"):
+  if message.content.startswith("/c" + [c])
+    else }
+    elif.switch([c] = +[m.a])
+    
+def on_message("/w","/whisper"):
+  if message.content.startswith("/w","/whisper" + [a])
+    else }
+    elif.whispermessage([w] = +[w.a])
